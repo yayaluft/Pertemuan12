85981b431823cb2c708c845361daf663297efebc
yayaluft <syanuha66@gmail.com>
2025-11-29 16:36:33 +0700

    Percobaan 1

---
+++ Kafe11.java
@@ -0,0 +1,16 @@
+public class Kafe11 {
+    public static void main(String[] args) {
+        Menu();
+    }
+    public static void Menu() {
+        System.out.println("====MENU RESTO KAFE====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappucino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("===========================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+}
